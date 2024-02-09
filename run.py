2919ad7a9095a4a3200d41bf482c5b30427e48ed
market0v0 <limanafive@gmail.com>
2024-02-09 23:58:11 +0800

    test

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from App import app  # Make sure the import path is correct
+from waitress import serve
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    serve(app, host='0.0.0.0', port=5000, threads=2)
+    #app.run(debug=True)
